862f8af6d267fcd28164111027d2b13adce14748
vaishnav <vaishnavvm.mec@gmail.com>
2025-03-16 16:52:26 +0530

    initial commit

---
+++ Scribble/src/main/java/com/screen/Scribble/ScribbleApplication.java
@@ -0,0 +1,13 @@
+package com.screen.Scribble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScribbleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScribbleApplication.class, args);
+	}
+
+}
---
+++ Scribble/src/test/java/com/screen/Scribble/ScribbleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.screen.Scribble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScribbleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
