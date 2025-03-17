0eeb9d4e3af03694f438fa6bfe794601d313f614
Vaishnav V M <74853284+vaishnavvilakkumadathil@users.noreply.github.com>
2025-03-17 21:27:17 +0530

    Update OmdbService.java

--- Scribble/src/main/java/com/screen/scribble/service/OmdbService.java
+++ Scribble/src/main/java/com/screen/scribble/service/OmdbService.java
@@ -1,30 +1,28 @@
 package com.screen.scribble.service;
 
-import com.screen.scribble.Repository.MongoRepo;
 import com.screen.scribble.config.OmdbConfig;
 import com.screen.scribble.model.LogModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-
-import java.util.List;
-import java.util.Optional;
-
 @Service
 public class OmdbService {
-
-    private OmdbConfig omdbConfig;
+    @Autowired(required = true)
     private RestTemplate restTemplate;
-    private final String key = omdbConfig.getKey();
-    private final String baseUrl = omdbConfig.getUrl();
 
-    public OmdbService(MongoRepo mongoRepo) {
+    private final OmdbConfig omdbConfig;
+    private final String key;
+    private final String baseUrl;
+
+    public OmdbService(OmdbConfig omdbConfig) {
+        this.omdbConfig = omdbConfig;
+        this.key = omdbConfig.getKey();
+        this.baseUrl = omdbConfig.getUrl();
     }
 
     public LogModel getMovieById(String id) {
-        String url = baseUrl + "?apikey=" + key + "&i=" + id;
+        String url = String.format("%s?apikey=%s&i=%s", baseUrl, key, id);
         return restTemplate.getForObject(url, LogModel.class);
     }
-
 }
