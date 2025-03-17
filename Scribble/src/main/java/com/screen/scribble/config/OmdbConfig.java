f50a16cf0ec8f90e2de091ce0ac013707004ee29
Vaishnav V M <74853284+vaishnavvilakkumadathil@users.noreply.github.com>
2025-03-17 21:26:51 +0530

    Update OmdbConfig.java

--- Scribble/src/main/java/com/screen/scribble/config/OmdbConfig.java
+++ Scribble/src/main/java/com/screen/scribble/config/OmdbConfig.java
@@ -4,17 +4,28 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties(prefix = "omdb-api")
+@ConfigurationProperties(prefix = "omdb.api")
 public class OmdbConfig {
 
     private String url;
     private String key;
 
+    public OmdbConfig() {
+    }
+
     public String getUrl() {
         return url;
     }
 
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
     public String getKey() {
         return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
     }
 }
